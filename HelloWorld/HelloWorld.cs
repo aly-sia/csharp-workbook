3d7b92438c7fe70871223f3553a9f822f2215a49
ALYSIA LOPEZ <alylopez85@gmail.com>
2019-04-02 20:25:41 -0500

    My csharp greetings program take1

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int [] numbers = {23, 7, 4, 3,11,8,9, 49};
+
+            if(numbers.Length ==0 ) {
+                Console.WriteLine("Your list is empty");
+            } else {
+                int largest = numbers[0];
+                for(int i=1;i<numbers.Length;i++){
+                    
+                }
+
+            }
+            }
         }
     }
-}
+
