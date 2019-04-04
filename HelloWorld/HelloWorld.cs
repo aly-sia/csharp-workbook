f39b23e043e7fe0f0410144c0e1605e516c2a34c
ALYSIA LOPEZ <alylopez85@gmail.com>
2019-04-04 18:54:05 -0500

    adding to helloworld

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HelloWorld my name is Bob");
+            Console.WriteLine("What is your name?");
+            String userInput = Console.ReadLine();
+            Console.WriteLine("Nice to meet you"+userInput);
+
         }
     }
 }
