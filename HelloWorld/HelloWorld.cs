6108744633838f0d7a97ca5248025a936ad1c85b
ALYSIA LOPEZ <alylopez85@gmail.com>
2019-04-08 11:56:13 -0500

    intro practice

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -16,6 +16,63 @@ namespace HelloWorld
          secondInt = "Console.ReadLine()";
          sum = "X + Y";
          Console.WriteLine(sum);
+
+         using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number");
+            int num1= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number");
+            int num2=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1+num2);
+
+
+            Console.Write("type in a number in yards to convert to inches");
+		    int numyards=Convert.ToInt32(Console.ReadLine());
+		    Console.WriteLine("numyards / 36");
+
+            bool people = true;
+            bool f = false;
+
+            decimal num = 0; 
+
+            string firstName = "Alysia";
+            string lastName = "Lopez";
+            string age = "26";
+            string job = "nanny";
+            string favoriteBand = "Cold Play";
+            string favoriteSportsTeam = "UT Longhorns";
+
+            Console.WriteLine(firstName);
+
+            decimal num = Convert.ToInt32(num);
+
+
+             int FirstNo, SecondNo, Sum, Product, Difference, Quotient, Remainder;
+        
+        FirstNo = 100;
+        
+        SecondNo = 10;
+        
+        Sum = FirstNo + SecondNo;
+        Product = FirstNo * SecondNo;
+        Difference = FirstNo - SecondNo;
+        Quotient = FirstNo / SecondNo;
+        
+        Console.Write("\n\nSum is : " + Sum);
+        Console.Write("\nProduct is : " + Product);
+        Console.Write("\nDifference is : " + Difference);
+        Console.Write("\nQuotient is : " + Quotient);	
+        }
+    }
+}
         }
     }
 }
