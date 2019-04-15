3595f7cb7ce411c204d48e4fa801b5a63cf6c143
ALYSIA LOPEZ <alylopez85@gmail.com>
2019-04-15 10:55:31 -0500

    pig latin

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -5,8 +5,17 @@ namespace PigLatin
     class Program
     {
         public static void Main()
-        {
-            // your code goes here
+         {
+            Console.WriteLine("Enter a word to be translated to pig Latin");
+			string word = Console.ReadLine();
+			string firstLetter = word.Substring(0, 1);
+			string restWord = word.Substring(1);
+			Console.WriteLine(restWord + firstLetter + "ay");
+			char[] vowel = { 'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y' };
+			for (int i = 0; i < vowel.Length; i++)
+			{
+				//some code????
+			}
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
